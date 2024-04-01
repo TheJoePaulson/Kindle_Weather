9f733d1cac72e209d69bec5835c455911a91ea65
Joe Paulson <joe.paulson@gmail.com>
2024-03-31 21:28:50 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-var api_locParams = "q=Nanjing";
+var api_locParams = "q=Denver";
 // or api_locParams = "q=Paris";
 var api_appId = "404f9e50d254b230661194dabb52b700";
-var api_lang = "zh_cn"
-var api_units = "metric";
+var api_lang = "en"
+var api_units = "imperial";
+// or metric
 
 var api_params = null;
 
